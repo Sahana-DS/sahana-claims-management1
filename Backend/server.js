125bc77dbdd5ba704346ce53cf999896a7b80cb4
Sahana-DS <sahana2000ds@gmail.com>
2025-07-09 11:27:44 +0530

    sahana-claims-management

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3604;
 
 // Middleware
 app.use(cors());
@@ -153,5 +153,5 @@ app.delete('/api/claims', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://13.60.8.19:${port}`);
+    console.log(`Server running at http://54.145.131.215:${port}`);
 });
